53627a128c32d5654d648e25e23762562d0ba023
Alison Maultasch <armaultasch@gmail.com>
2016-06-20 09:35:42 -0400

    everything

--- day5/lib/bishop.rb
+++ day5/lib/bishop.rb
@@ -2,16 +2,25 @@ require_relative("pieces.rb")
 
 class Bishop < Piece 
  	def can_move?(final_x, final_y)
-		if (@x+1 == final_x) && (@y+1 == final_y)
-			 true
-		elsif (@x-1 == final_x) && (@y-1 == final_y)
-			 true
-		elsif (@x-1 == final_x) && (@y+1 == final_y)
-			 true
-		elsif (@x+1 == final_x) && (@y-1 == final_y)
-			 true	 
+ 		change_x = (final_x - @x).abs
+		change_y = (final_y - @y).abs
+		if change_x == change_y
+			true
 		else
 			false
 		end
 	end
 end
+# 		if (@x+1 == final_x) && (@y+1 == final_y)
+# 			 true
+# 		elsif (@x-1 == final_x) && (@y-1 == final_y)
+# 			 true
+# 		elsif (@x-1 == final_x) && (@y+1 == final_y)
+# 			 true
+# 		elsif (@x+1 == final_x) && (@y-1 == final_y)
+# 			 true	 
+# 		else
+# 			false
+# 		end
+# 	end
+# end
--- day5/lib/queen.rb
+++ day5/lib/queen.rb
@@ -2,17 +2,26 @@ require_relative("pieces.rb")
 
 class Queen < Piece
 	def can_move?(final_x, final_y)
-		if @x == final_x
-			true
-		elsif @y == final_y
-			true
-		elsif (@x-1 == final_x) && ((@y-1 == final_y) || @y+1 == final_y)
-			true
-		elsif (@x+1 == final_x) && ((@y-1 == final_y) || @y+1 == final_y)
-			true
+		change_x = (final_x - @x).abs
+		change_y = (final_y - @y).abs
+		if ((@x == final_x) || (@y == final_y)) || (change_x == change_y)
+			 true
 		else
 			false
 		end
 	end
 end
+# 		if @x == final_x
+# 			true
+# 		elsif @y == final_y
+# 			true
+# 		elsif (@x-1 == final_x) && ((@y-1 == final_y) || @y+1 == final_y)
+# 			true
+# 		elsif (@x+1 == final_x) && ((@y-1 == final_y) || @y+1 == final_y)
+# 			true
+# 		else
+# 			false
+# 		end
+# 	end
+# end
 			
